2690a126e7e3dac71fecc1201837b57c0811b425
fengT-T <2995207228@qq.com>
2020-05-06 23:23:13 +0800

    module

--- docs/assets/js/8.9d4c4266.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{53:function(t,s,e){"use strict";e.r(s);var n=e(0),o=Object(n.a)({},function(){var t=this.$createElement,s=this._self._c||t;return s("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[s("h2",{staticClass:"text-center"},[this._v("CoCoMusic")])])},[],!1,null,null,null);s.default=o.exports}}]);
